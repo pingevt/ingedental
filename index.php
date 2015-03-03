0f342c1b78aba9809c8214ac38d720700c1fab52
Pete Inge <pete.inge@gmail.com>
2015-03-03 09:53:56 -0500

    Initial commit
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>Inge Dental</title>
+		
+		<?php include('assets/include/head.php'); ?>
+		
+		<script src="assets/js/colHeightFix.js" type="text/javascript" ></script>
+		<script src="assets/js/rotate.js" type="text/javascript" ></script>
+		
+		<link rel="stylesheet" type="text/css" href="assets/js/shadowbox/shadowbox.css" />
+		<script type="text/javascript" src="assets/js/shadowbox/shadowbox.js"></script>
+		<script type="text/javascript" src="assets/js/home.js" ></script>
+
+	</head>
+	
+	<body onload=" go();">
+		<div id="bg-overlay">&nbsp;</div> <!-- END bg-overlay -->
+		
+		<div id="mainColDrop">
+			<div id="mainCol">
+				
+				<div id="headerBanner"></div> <!-- END headerBanner -->
+				<div id="linkBack" onclick="location.href = '/';"></div> <!-- END linkBack --> 
+				<div id="linkBack2" onclick="location.href = '/';"></div> <!-- END linkBack2 --> 
+				
+				<div id="swoosh"></div> <!-- END swoosh -->
+				
+				<div id="navCol">
+					<div id="nav">
+						<?php include('assets/include/nav.php'); ?>
+					</div> <!-- END nav -->
+					<div id="navBottom">&nbsp;</div> <!-- END navBottom -->
+				</div> <!-- END navCol -->
+				
+				<div id="mainContentCol">
+					
+					<div id="FProtate">
+						<!-- <div id="clickable" onclick="loadClickable()"></div> -->
+						<div id="" onclick=""></div>
+						<div id="r1" class="rotateImg"></div>
+						<div id="r2" class="rotateImg"></div>
+						<div id="r3" class="rotateImg"></div>
+					</div>
+					
+					<div id="contentWrapper">
+						<div id="topShadow"></div> <!-- END topShadow -->
+						
+						<div id="content">
+<!-- ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== -->
+
+<h1>Welcome to our website!</h1>
+
+<p>We hope this site will be more than just an informative introduction to our dental office. There are many unique features of our office that we are very excited about. We offer state of the art dental technology with a very human touch. After all, our patients are terrific people, and they deserve terrific dentistry!</p>
+
+<p>It is our goal to help our patients enjoy the benefits of a lifetime of good oral health and to provide them with a smile they can really smile about. Today's dentistry is both art and science and makes this goal possible for practically everyone. Take a look at the rest of our website and see how.</p>
+
+
+
+
+
+<!-- ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== -->
+						</div> <!-- END content -->
+						<?php include('assets/include/footer.php'); ?>
+					</div> <!-- END contentWrapper" -->
+				</div> <!-- End mainContentCol -->
+				
+				<div class="clear"></div>
+			</div> <!-- End mainCol -->
+		</div> <!-- End MainColDrop -->
+		<script type="text/javascript">columnHeightFix();</script>
+	</body>
+</html>
